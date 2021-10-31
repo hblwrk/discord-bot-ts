7cb8e886bba341d1dffc38d2edff635ab4dfc93e
mheiland <15824364+mheiland@users.noreply.github.com>
2021-10-31 16:44:59 +0100

    removing untracked files

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
-// Require the necessary discord.js classes
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+// Dependencies
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
 
-const secrets = require('./modules/secrets');
-const token = secrets.read('discord_token')
+// Read secrets
+const secrets = require("./modules/secrets");
+const token = secrets.read("discord_token")
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-//
-// Add Folder redirects
-//
+
 // Redirect for commands
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // Redirect for events
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
-///////////////////////
-
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+  }
 });
 
 // Login to Discord with your client's token
--- modules/secrets.js
+++ modules/secrets.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const secrets = {};
 
 secrets.read = function read(secretName) {
   try {
     // Attempting to fetch Docker secret
-    return fs.readFileSync(`/run/secrets/${secretName}`, 'utf8');
+    return fs.readFileSync(`/run/secrets/${secretName}`, "utf8");
   } catch(err) {
     // Fall back to config.json in case Docker secret is unavailable
-    const keys = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+    const keys = JSON.parse(fs.readFileSync("config.json", "utf8"))
     return getValueFromJSONConfig(keys, secretName);
   }
 };
