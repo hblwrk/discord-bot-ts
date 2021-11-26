58783a96f8162508b1dc7603f4bcc0ae4a91616d
mheiland <15824364+mheiland@users.noreply.github.com>
2021-11-26 08:43:39 +0100

    google

--- modules/lmgtfy.ts
+++ modules/lmgtfy.ts
@@ -2,3 +2,8 @@ export function lmgtfy(search: string) {
   const link = `<http://letmegooglethat.com/?q=${encodeURIComponent(search)}>`;
   return link;
 }
+
+export function google(search: string) {
+  const link = `<https://www.google.com/search?q=${encodeURIComponent(search)}>`;
+  return link;
+}
