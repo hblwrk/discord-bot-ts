4971b3f74dc2bd2cc3b2ea0420bed50ee3277111
mheiland <15824364+mheiland@users.noreply.github.com>
2026-02-18 20:20:49 +0100

    chore: log healthcheck port on startup

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 /* eslint-disable import/extensions */
 import {getLogger} from "./modules/logging.js";
+import {readSecret} from "./modules/secrets.js";
 import {startBot} from "./modules/startup-orchestrator.js";
 
 const logger = getLogger();
 console.log(`Started with loglevel: ${logger.level}`);
+const configuredPort = Number.parseInt(readSecret("healthcheck_port").trim(), 10);
+const healthcheckPort = Number.isNaN(configuredPort) ? 11312 : configuredPort;
+console.log(`Healthcheck port: ${healthcheckPort}`);
 
 void startBot({
   logger,
