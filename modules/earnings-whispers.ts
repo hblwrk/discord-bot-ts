71645798517a1a68cdf26598f837a9c880a9d818
mheiland <15824364+mheiland@users.noreply.github.com>
2026-05-06 21:23:40 +0200

    fix CodeQL double escaping (#1707)

--- modules/earnings-whispers.ts
+++ modules/earnings-whispers.ts
@@ -225,11 +225,11 @@ function normalizeEarningsWhispersSourceText(value: string): string {
 
 function decodeHtmlEntities(value: string): string {
   return value
-    .replace(/&amp;/g, "&")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&quot;/g, "\"")
-    .replace(/&#39;/g, "'");
+    .replace(/&#39;/g, "'")
+    .replace(/&amp;/g, "&");
 }
 
 function normalizeEarningsWhispersTicker(value: string): string {
